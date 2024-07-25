6233d2daef06b31eee6455507948001588062b0e
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-25 18:24:38 -0400

    feat: angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
 import { provideFirebase } from "./firebase-modules";
+import {
+  provideAnimationsAsync
+} from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     provideRouter(routes),
     provideFirebase(),
   ],
