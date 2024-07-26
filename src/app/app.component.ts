ddf81cc578bdf24ae92d1a7c16aaf93afb35655c
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-25 21:05:30 -0400

    refactor: simple welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatButton, MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
+import { GenericItem } from "./shared/interfaces/generic-item";
+import { RouterLink } from "@angular/router";
+import { NgTemplateOutlet } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbar, MatIconButton, MatIcon,
+    MatCard, MatCardHeader, MatCardContent,
+    MatCardActions, MatButton, MatCardTitle,
+    MatCardSubtitle, MatMenu, MatMenuItem,
+    RouterLink, MatMenuTrigger, NgTemplateOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-template';
+  title = 'Angular Template';
+
+  menuItems: GenericItem[] = [
+    {
+      id: 'angular',
+      name: 'Angular',
+      href: 'https://angular.dev/',
+      icon: 'link',
+    },
+    {
+      id: 'angular-material',
+      name: 'Angular Material',
+      href: 'https://material.angular.io/',
+      icon: 'link',
+    },
+    {
+      id: 'firebase',
+      name: 'Firebase',
+      href: 'https://firebase.google.com/',
+      icon: 'link',
+    },
+    {
+      id: 'material-icons',
+      name: 'Material Icons',
+      href: 'https://fonts.google.com/icons',
+      icon: 'link',
+    },
+  ];
 }
